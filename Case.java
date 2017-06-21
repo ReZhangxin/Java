7b4e7030a2f87212045d31fcb77cd3d9a593b110
Railgunx <1307881527@qq.com>
2017-06-21 23:34:37 +0800

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * Case
+ * 创建人:zhangss
+ * QQ:1307881527
+ * 时间：2017年6月13日-下午9:50:53 
+ * @version 1.0.0
+ * 
+ */
+package com.java03;
+
+/**
+ * 
+ * Case
+ * 创建人:zhangss
+ * QQ:1307881527
+ * 时间：2017年6月13日-下午9:50:53 
+ * @version 1.0.0
+ * 
+ */
+public class Case {
+	public static void main(String[] args){
+		//一个汉字占两个字节
+		String s = "我";
+		System.out.println(s.getBytes().length);
+		String s2 ="A";
+		System.out.println(s2.getBytes().length);
+	}	
+}
